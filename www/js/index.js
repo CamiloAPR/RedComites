fd13fc1b8d0d52bf67bf6d19b2fbcf79de7c8b36
CamiloAPR <palaciocamilor@gmail.com>
2017-05-18 14:20:08 -0500

    preparar index(no es un avance considerable)

---
+++ www/js/index.js
@@ -0,0 +1,17 @@
+$(document).on("pageshow","#main",function(){
+    var htmlTemplate="<li>"+
+    			"<div class='ui-block-a itemGrid' style='background-color: $color'>"+
+                      "<a href=''><img  src='$icon' alt=''></a>"+
+                      "<h6>$name</h6>"+
+                    "</div>";
+              "</li>";
+
+    var lista = $("#lista");
+    var dataTemplate = [{field: "color"}, {field: "icon"},{field: "name"}]
+    lista.empty();
+    var callback = function(data){
+      return ux_service.createHTMLComponents(htmlTemplate, dataTemplate, lista, data);
+    }
+
+    db_service.get("/committee", callback);
+});
